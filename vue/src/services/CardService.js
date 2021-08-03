cedc57fe590747311586c9505fcc8dbeb90770d2
Luis Avellana <luisavellana@gmail.com>
2021-08-03 16:49:35 -0400

    add a return silly!

--- vue/src/services/CardService.js
+++ vue/src/services/CardService.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 
 export default {
-    getAllCards(){
+    getAllCards() {
         return axios.get('/cards')
     },
     getCard() {
         return axios.get('/cards/:id')
     },
-    getCardByDeck(deckId){
+    getCardByDeck(deckId) {
         return axios.get(`/decks/${deckId}/cards`)
+    },
+    createCard() {
+        return axios.post('/cards', {
+            question: 'does vue work?',
+            answer: 'maybe?',
+            keywords: 'dumb'
+        })
     }
+
 }
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,58 +1,53 @@
 <template>
-    <div class="mainappbody">
-        <Nav />
-        <DeckList :decks="decks" 
-            @add-new-deck="addNewDeck" 
-            @get-deck="getDeck" 
-        />
-        <CardList :cards="cards" />
-    </div>
+  <div class="mainappbody">
+    <Nav />
+    <DeckList :decks="decks" @add-new-deck="addNewDeck" @get-deck="getDeck" />
+    <CardList :cards="cards" />
+  </div>
 </template>
 
 <script>
-    import Nav from '../components/Nav'
-    import DeckList from '../components/DeckList'
-    import CardList from '../components/CardList'
-    import DeckService from '../services/DeckService'
-    import CardService from '../services/CardService'
+import Nav from "../components/Nav";
+import DeckList from "../components/DeckList";
+import CardList from "../components/CardList";
+import DeckService from "../services/DeckService";
+import CardService from "../services/CardService";
 
-    export default {
-        name: 'Home',
-        components: {
-            DeckList,
-            CardList,
-            Nav
-        },
-        data(){
-            return {
-                decks: [],
-                cards: []
-            }
-        },
-        methods:{
-            getDeck(deckId){
-                CardService.getCardByDeck(deckId).then(resp => {
-                    this.cards = resp.data
-                })
-            },
-            addNewDeck(){
-                CardService.getAllCards().then(resp => {
-               this.cards = resp.data
-           });
-            }
-        },
-        created(){
-           DeckService.getAllDecks().then(resp => {
-               this.decks = resp.data
-           });
-        }
-
-        
-    }
+export default {
+  name: "Home",
+  components: {
+    DeckList,
+    CardList,
+    Nav,
+  },
+  data() {
+    return {
+      decks: [],
+      cards: [],
+    };
+  },
+  methods: {
+    getDeck(deckId) {
+      CardService.getCardByDeck(deckId).then((resp) => {
+        this.cards = resp.data;
+      });
+    },
+    addNewDeck() {
+      CardService.createCard().then((resp) => {
+        console.log(resp);
+      });
+    },
+  },
+  created() {
+    DeckService.getAllDecks().then((resp) => {
+      this.decks = resp.data;
+    });
+  },
+};
 </script>
 
 <style scoped>
-.mainappbody{
-    display: flex;
+.mainappbody {
+  display: flex;
 }
 </style>
