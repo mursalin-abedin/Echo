ccea913c69e723cb6589a1d2a03d5f319aa67e62
Andrew Ball <andrewgball93@gmail.com>
2021-08-05 16:32:37 -0400

    worked on startstudysession

--- vue/src/components/StudySessionComponents/StartStudySession.vue
+++ vue/src/components/StudySessionComponents/StartStudySession.vue
@@ -1,28 +1,84 @@
 <template>
   <div>
     <div class="header">
-        <div class="icon"><i class="fas fa-user-astronaut echoicon"></i></div>
-        <div class="echotext">Echo</div>
-      
+      <div class="icon"><i class="fas fa-user-astronaut echoicon"></i></div>
+      <div class="echotext">Echo</div>
     </div>
-
-    <div class="pleasework">
-      <!-- <h2>SELECT A DECK</h2> -->
+    <div class="page">
+      <div class="pleasework">
+        <h1>Select a deck to study from!</h1>
+      </div>
+      <div class="decks">
+        <div v-for="deck in decks" :key="deck.deckId">
+          <div class="deck">
+            <div class="deckname">{{ deck.deckName }}</div>
+          </div>
+        </div>
+      </div>
     </div>
-    
   </div>
 </template>
 
 <script>
-export default {};
+import DeckService from "@/services/DeckService";
+export default {
+  data() {
+    return {
+      decks: Array,
+    };
+  },
+  methods: {},
+  created() {
+    DeckService.getAllDecks().then((resp) => {
+      this.decks = resp.data;
+    });
+  },
+};
 </script>
 
 <style scoped>
 
+.decks {
+  margin-top: 190px;
+  display: inline-flex;
+  justify-content: center;
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.deck {
+  display: block;
+  background-color: lightgray;
+  width: 300px;
+  height: 300px;
+  border: 2px solid darkgray;
+  margin: 15px 30px;
+  border-radius: 10px;
+
+  display: flex;
+  justify-content: center;
+
+  box-shadow: 10px 5px 5px darkgray;
+}
+
+.deckname {
+  margin-top: 15px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 30pt;
+  text-align: center;
+}
+
 .pleasework {
-    margin-top: 81px;
-    text-align: center;
-    justify-content: center;
+  margin-top: 81px;
+  text-align: center;
+  width: auto;
+  display: flex;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  font-size: 14pt;
 }
 
 .header {
